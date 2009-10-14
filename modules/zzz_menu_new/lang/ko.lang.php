6e46a87cc2c793a1f805b143492d1208a8a8a175
sinsy200 <sinsy200@7d98a5b0-df5e-2b15-0514-3bde50a915e2>
2009-10-14 02:41:18 +0000

    [메뉴에 새글 표시 모듈] Ver. 0.0.1 trunk

---
+++ modules/zzz_menu_new/lang/ko.lang.php
@@ -0,0 +1,27 @@
+<?php
+    /**
+     * @file   modules/zzz_menu_new/lang/ko.lang.php
+     * @author 난다날아 <sinsy200@gmail.com>
+     * @brief  메뉴에 새글 표시(zzz_menu_new) 모듈의 기본 언어팩
+     **/
+
+    $lang->menu_new = '메뉴에 새글 표시';
+    
+    $lang->use_menu_new = '메뉴에 새글 표시 사용';
+    $lang->about_use_menu_new = '메뉴에 새글 표시 기능을 사용합니다.';
+
+    $lang->use_comment = '새 댓글에 대해서도 동작';
+    $lang->about_use_comment = '해당 메뉴의 새 댓글에 대해서도 작동하도록 합니다.';
+    
+    $lang->duration_new = 'new 표시 시간(hours)';
+    $lang->about_duration_new = '새로 등록된 게시물의 new 표시시간을 정할 수 있습니다. (기본 24시간)';
+    
+    $lang->up_new = '올림 표시';
+    $lang->about_up_new = '새 게시물이 있는 메뉴의 상위 메뉴에도 모두 new 표시합니다.';
+    
+    $lang->text_new = 'text 변수에도 이미지 표시';
+    $lang->about_text_new = '메뉴의 text 변수에도 이미지를 표시합니다.';
+    
+    $lang->new_image = 'new 이미지 주소';
+    $lang->about_new_image = 'new 이미지 주소를 입력하세요. 비워두면 기본 이미지를 이용합니다.';
+?>
---
+++ modules/zzz_menu_new/tpl/js/zzz_menu_new.admin.js
@@ -0,0 +1,4 @@
+/* 캐시파일 재생성 */
+function doRemakeCache() {
+    exec_xml('zzz_menu_new', 'procZzz_menu_newAdminRemakeCacheAll', new Array(), null, new Array());
+}
