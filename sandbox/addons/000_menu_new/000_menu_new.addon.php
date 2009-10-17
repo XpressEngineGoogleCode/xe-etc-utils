704e3c70775f38a55c01ea5b24d944d32a02b1c4
sinsy200 <sinsy200@7d98a5b0-df5e-2b15-0514-3bde50a915e2>
2009-10-17 12:22:21 +0000

    [메뉴에 새글 표시 애드온] #18380256 CafeXE 연동

--- sandbox/addons/000_menu_new/000_menu_new.addon.php
+++ sandbox/addons/000_menu_new/000_menu_new.addon.php
@@ -9,7 +9,15 @@
      **/
 
     // 메뉴 트리 재생성시
-    if ($called_position == 'after_module_proc' && strpos($this->act, 'procMenuAdmin') !== false) {
+    if ($called_position == 'after_module_proc' && in_array($this->act, array(
+            'procHomepageInsertMenuItem',
+            'procHomepageDeleteMenuItem',
+            'procHomepageMenuItemMove',
+            'procMenuAdminInsertItem',
+            'procMenuAdminDeleteItem',
+            'procMenuAdminMoveItem',
+            'procMenuAdminMakeXmlFile'
+        ))) {
         $oMenuNewAdminController = &getAdminController('zzz_menu_new');
         $menu_srl = Context::get('menu_srl');
         if (!$menu_srl)  return;
@@ -17,4 +25,21 @@
         // 새글 표시도 재생성!
         $oMenuNewAdminController->procZzz_menu_newAdminRemakeCache($menu_srl);
     }
+
+    // 카페 메뉴 설정 화면에 메뉴에 새글 표시 설정도 표시
+    if ($called_position == 'before_display_content' && Context::getResponseMethod() == 'HTML' && Context::get('act') == 'dispHomepageTopMenu') {
+        // 설정 가져오기
+        $oMenuNewModel = &getModel('zzz_menu_new');
+        $config = $oMenuNewModel->getConfig();
+        Context::set('config', $config);
+        
+        // 설정 화면 컴파일
+        $oTemplate = new TemplateHandler();
+        $menu_new = $oTemplate->compile('./modules/zzz_menu_new/tpl', 'menu_new_config.html');
+        $menu_new = preg_replace_callback('/<!--Meta:([a-z0-9\_\/\.\@]+)-->/is', array($this,'transMeta'), $menu_new);
+        
+        // HTML 추가
+        $output = str_replace('</iframe>', "</iframe>$menu_new", $output);
+    }
+    
 ?>
