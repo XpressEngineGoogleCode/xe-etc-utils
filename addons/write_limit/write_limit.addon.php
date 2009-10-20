00e927e991e5d18cd9674caf18a520cf17683858
sinsy200 <sinsy200@7d98a5b0-df5e-2b15-0514-3bde50a915e2>
2009-10-20 01:32:09 +0000

    [하루 글/댓글 작성량 제한 애드온] #18384706 게시판 메시지 있을 경우 애드온 실행 취소, 메시지에 다국어 가능하도록

--- addons/write_limit/write_limit.addon.php
+++ addons/write_limit/write_limit.addon.php
@@ -20,52 +20,64 @@
 	if (!logged_info)	$args->ipaddress = 	$_SERVER['REMOTE_ADDR'];
 	else				$args->member_srl = $logged_info->member_srl;
 
-	// 글 작성 화면
-	if($called_position == 'before_module_init' && ($this->act == 'dispBoardWrite' || $this->act == 'procBoardInsertDocument') ) {
+    Context::loadLang(_XE_PATH_.'addons/write_limit/lang');
+    
+	// 글 작성시
+	if($called_position == 'before_module_init' && $this->act == 'procBoardInsertDocument') {
 		// 제한이 걸려있지 않으면 통과!
 		if (!$addon_info->document_limit)	return;
 		
 		$output = executeQuery('addons.write_limit.document_count', $args);
 
 		if (!$output->toBool()) {
-			if ($this->act = 'dispBoardWrite'){
-				$this->error = "SQL Error";
-				return;
-			}else{
-				// xml_rpc return
-				header("Content-Type: text/xml; charset=UTF-8");
-				header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-				header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-				header("Cache-Control: no-store, no-cache, must-revalidate");
-				header("Cache-Control: post-check=0, pre-check=0", false);
-				header("Pragma: no-cache");
-				print("<response>\r\n<error>-1</error>\r\n<message>SQL Error</message>\r\n</response>");
-	
-				Context::close();
-				exit();
-			}
+			// xml_rpc return
+			header("Content-Type: text/xml; charset=UTF-8");
+			header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+			header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+			header("Cache-Control: no-store, no-cache, must-revalidate");
+			header("Cache-Control: post-check=0, pre-check=0", false);
+			header("Pragma: no-cache");
+			print("<response>\r\n<error>-1</error>\r\n<message>SQL Error</message>\r\n</response>");
+
+			Context::close();
+			exit();
 		}
 
 		// 설정된 개수 이상의 작성이면 중단!
 		if ($output->data->count >= $addon_info->document_limit) {
-			if ($this->act = 'dispBoardWrite'){
-				$this->error = "하루 작성량({$addon_info->document_limit}개)초과입니다.";
-				return;
-			}else{
-				// xml_rpc return
-				header("Content-Type: text/xml; charset=UTF-8");
-				header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-				header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-				header("Cache-Control: no-store, no-cache, must-revalidate");
-				header("Cache-Control: post-check=0, pre-check=0", false);
-				header("Pragma: no-cache");
-				printf("<response>\r\n<error>-1</error>\r\n<message>하루 작성량(%d개)초과입니다.</message>\r\n</response>", $addon_info->document_limit);
-	
-				Context::close();
-				exit();
-			}
+			// xml_rpc return
+			header("Content-Type: text/xml; charset=UTF-8");
+			header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+			header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+			header("Cache-Control: no-store, no-cache, must-revalidate");
+			header("Cache-Control: post-check=0, pre-check=0", false);
+			header("Pragma: no-cache");
+			printf("<response>\r\n<error>-1</error>\r\n<message>".Context::getLang('msg_limit_document')."</message>\r\n</response>", $addon_info->document_limit);
+
+			Context::close();
+			exit();
 		}
 
+    // 글 작성 화면
+    }else if($called_position == 'after_module_proc' && $this->act == 'dispBoardWrite') {
+        // 제한이 걸려있지 않으면 통과!
+		if (!$addon_info->document_limit)	return;
+
+        // 게시판에서 메시지가 나가면 중단
+        if ($this->getTemplateFile() == 'message.html') return;
+        
+		$db_output = executeQuery('addons.write_limit.document_count', $args);
+
+		if (!$db_output->toBool()) {
+			$this->errer = "SQL Error";
+			return;
+		}
+
+		// 설정된 개수 이상의 작성이면 중단!
+		if ($db_output->data->count >= $addon_info->document_limit) {
+			$output = new Object(-1, sprintf(Context::getLang('msg_limit_document'), $addon_info->document_limit));
+			return;
+		}
 	// 댓글 작성 때
     }else if($called_position == 'before_module_init' && $this->act == 'procBoardInsertComment') {
 
@@ -97,7 +109,7 @@
 			header("Cache-Control: no-store, no-cache, must-revalidate");
 			header("Cache-Control: post-check=0, pre-check=0", false);
 			header("Pragma: no-cache");
-			printf("<response>\r\n<error>-1</error>\r\n<message>하루 댓글 작성량(%d개)초과입니다.</message>\r\n</response>", $addon_info->comment_limit);
+			printf("<response>\r\n<error>-1</error>\r\n<message>".Context::getLang('msg_limit_comment')."</message>\r\n</response>", $addon_info->comment_limit);
 
 			Context::close();
 			exit();
