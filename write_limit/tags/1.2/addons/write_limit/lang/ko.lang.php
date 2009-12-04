2b49ee143f1453311d292aabee2d02861aa4491a
sinsy200 <sinsy200@7d98a5b0-df5e-2b15-0514-3bde50a915e2>
2009-12-04 07:52:26 +0000

    [작성량 제한]1.2 tag

---
+++ write_limit/tags/1.2/addons/write_limit/lang/ko.lang.php
@@ -0,0 +1,10 @@
+<?php
+    /**
+     * @file   ko.lang.php
+     * @author 난다날아 (sinsy200@gmail.com)
+     * @brief  한국어 언어팩
+     **/
+
+    $lang->msg_limit_document = '하루 작성량(%d개)초과입니다.';
+    $lang->msg_limit_comment = '하루 댓글 작성량(%d개)초과입니다.';
+?>
