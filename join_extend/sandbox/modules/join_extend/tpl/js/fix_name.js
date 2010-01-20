2817c0b5ac6e93aeb098415bd18d6709c8e9b031
sinsy200 <sinsy200@7d98a5b0-df5e-2b15-0514-3bde50a915e2>
2010-01-20 12:25:11 +0000

    [회원가입 확장]XE Core 1.4.0에서 수정금지 항목 전송이 안 되는 문제 수정

--- join_extend/sandbox/modules/join_extend/tpl/js/fix_name.js
+++ join_extend/sandbox/modules/join_extend/tpl/js/fix_name.js
@@ -1,14 +1,14 @@
 jQuery(function($) {
-    jQuery("input[name=user_name]").val(user_name).attr("disabled", "disabled");
+    jQuery("input[name=user_name]").val(user_name).attr("readonly", "readonly");
     
     try{
-        jQuery("#date_birthday").val(birthday).attr("disabled", "disabled");
+        jQuery("#date_birthday").val(birthday).attr("readonly", "readonly");
         jQuery("#date_birthday ~ .inputDate").val(birthday2);
     }catch(e){}
     
     try{
-        jQuery("input[name="+sex_var_name+"]").attr("disabled", "disabled");
-        jQuery("select[name="+sex_var_name+"]").attr("disabled", "disabled");
+        jQuery("input[name="+sex_var_name+"]").attr("readonly", "readonly");
+        jQuery("select[name="+sex_var_name+"]").attr("readonly", "readonly");
         
         jQuery("input[name="+sex_var_name+"][value="+sex+"]:radio").attr('checked','checked');
         jQuery("select[name="+sex_var_name+"] > option[value="+sex+"]").attr('selected','selected');
@@ -16,12 +16,12 @@ jQuery(function($) {
     }catch(e){}
     
     try{
-        jQuery("input[name="+age_var_name+"]").attr("disabled", "disabled");
+        jQuery("input[name="+age_var_name+"]").attr("readonly", "readonly");
         jQuery("input[name="+age_var_name+"]").val(age);
     }catch(e){}
 
     try{
-        jQuery("input[name="+recoid_var_name+"]").attr("disabled", "disabled");
+        jQuery("input[name="+recoid_var_name+"]").attr("readonly", "readonly");
     }catch(e){}
 
     try{
--- join_extend/sandbox/modules/join_extend/tpl/js/no_mod.js
+++ join_extend/sandbox/modules/join_extend/tpl/js/no_mod.js
@@ -3,29 +3,29 @@ jQuery(function($) {
         switch (no_mod_type[i]) {
             case 'text': case 'homepage': case 'email_address': case 'tel':
                 if (jQuery("input[name="+no_mod[i]+"]").val())
-                    jQuery("input[name="+no_mod[i]+"]").attr("disabled", "disabled");
+                    jQuery("input[name="+no_mod[i]+"]").attr("readonly", "readonly");
                 break;
             case 'radio': case 'checkbox':
                 if (jQuery("input[name="+no_mod[i]+"]").attr("checked"))
-                    jQuery("input[name="+no_mod[i]+"]").attr("disabled", "disabled");
+                    jQuery("input[name="+no_mod[i]+"]").attr("readonly", "readonly");
                 break;
             case 'select':
                 if (jQuery("select[name="+no_mod[i]+"]").val())
-                    jQuery("select[name="+no_mod[i]+"]").attr("disabled", "disabled");
+                    jQuery("select[name="+no_mod[i]+"]").attr("readonly", "readonly");
                 break;
             case 'kr_zip':
                 if (jQuery("input[name="+no_mod[i]+"]").val()) {
-                    jQuery("input[name="+no_mod[i]+"]").attr("disabled", "disabled");
+                    jQuery("input[name="+no_mod[i]+"]").attr("readonly", "readonly");
                     jQuery("input[name="+no_mod[i]+"] ~ a").css('display', 'none');
                 }
                 break;
             case 'date':
                 if (jQuery("input[name="+no_mod[i]+"]").val())
-                    jQuery("input[name="+no_mod[i]+"] ~ .inputDate").attr("disabled", "disabled").removeClass('inputDate');
+                    jQuery("input[name="+no_mod[i]+"] ~ .inputDate").attr("readonly", "readonly").removeClass('inputDate');
                 break;
             case 'textarea':
                 if (jQuery("textarea[name="+no_mod[i]+"]").val())
-                    jQuery("textarea[name="+no_mod[i]+"]").attr("disabled", "disabled");
+                    jQuery("textarea[name="+no_mod[i]+"]").attr("readonly", "readonly");
         }
     }
 });
