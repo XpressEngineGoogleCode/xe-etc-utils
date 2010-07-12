ae1ff204299a456eaeb5375ec8f85b629c8475bd
sinsy200 <sinsy200@7d98a5b0-df5e-2b15-0514-3bde50a915e2>
2010-07-12 11:59:26 +0000

    [비회원 이름 고정] disabled를 readonly로 변경

--- guest_name/sandbox/addons/guest_name/guest_name.js
+++ guest_name/sandbox/addons/guest_name/guest_name.js
@@ -1,3 +1,3 @@
 jQuery(function($) {
-    jQuery("input[name=nick_name]").val(guest_name).attr("disabled", "disabled");
+    jQuery("input[name=nick_name]").val(guest_name).attr("readonly", "readonly");
 });
